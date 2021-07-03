f092fb26ea14f6075cfcbfddded0c446f1e694f7
sofiapel <sofiacarlapellegrini@gmail.com>
2021-07-03 13:58:57 -0300

    funca el axios

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,8 +1,23 @@
 const { Router } = require('express');
+const axios = require('axios');
+
 const router = Router()
 
-router.get('/', (req,res) =>{
-    res.send('locura')
+router.get('/', async(_req,res) =>{
+    try {
+        const countries = await axios.get('https://restcountries.eu/rest/v2/all')
+        // console.log(countries)
+        console.log(countries.data)
+        res.json(countries.data)
+        
+    } catch(err) {
+        console.log('jaja entraba aca')
+        res.send(err)
+
+    }
+    
+    
+    //res.send('locura')
 })
 
 module.exports = router;
