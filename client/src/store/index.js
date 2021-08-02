23c7eff6e832254a408305ac4f5ac28f81a8793b
sofiapel <sofiacarlapellegrini@gmail.com>
2021-08-01 21:23:40 -0300

    finish

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
 
-
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk)
